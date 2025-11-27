6bc59cb5f2b6d363b4cb42021a51178574113703
basiccat0 <yunyaol@nycstudents.net>
2025-11-26 21:47:06 -0500

    Add files via upload

--- src/Game.java
+++ src/Game.java
@@ -17,70 +17,73 @@ public class Game {
                 int randomNum = (int) (Math.random() * 5) + 1;
                 System.out.println("Alright now choose a number between 1 and 5");
                 int chosenNum = scan.nextInt();
-                if (chosenNum != randomNum && (chosenNum == 1 || chosenNum == 2 || chosenNum == 3 || chosenNum == 4 || chosenNum == 5)) {
-                    System.out.println("Again!");
-                } else {
-                    System.out.println("You chose the wrong number! Game Over :(");
-                    System.out.println("You won absolutely nothing...");
+                if (chosenNum == randomNum || chosenNum < 1 || chosenNum > 5) {
+                    System.out.println("You chose the wrong number! You won literally nothing");
+                    System.out.println("Game Over :(");
                     System.exit(0);
                 }
-                loop --;
-            } else if (loop == 6 || loop == 5 || loop == 4) {
+                System.out.println("Again!");
+                loop--;
+
+                } else if (loop == 6 || loop == 5 || loop == 4) {
                 int randomNum = (int) (Math.random() * 4) + 1;
                 System.out.println("Now choose a number between 1 and 4");
                 int chosenNum = scan.nextInt();
-                if (chosenNum != randomNum && (chosenNum == 1 || chosenNum == 2 || chosenNum == 3 || chosenNum == 4)) {
-                    System.out.println("Again!");
-                } else {
-                    System.out.println("You chose the wrong number! Game Over :(");
-                    System.out.println("You won absolutely nothing...");
+                if (chosenNum == randomNum || chosenNum < 1 || chosenNum > 4) {
+                    System.out.println("You chose the wrong number! You walk away with $1,000");
+                    System.out.println("Game Over :(");
                     System.exit(0);
                 }
-                loop --;
-            } else if (loop == 3 || loop == 2) {
+
+                System.out.println("Again!");
+                loop--;
+
+                } else if (loop == 3 || loop == 2) {
                 int randomNum = (int) (Math.random() * 3) + 1;
                 System.out.println("Choose a number between 1 and 3");
                 int chosenNum = scan.nextInt();
-                if (chosenNum != randomNum && (chosenNum == 1 || chosenNum == 2 || chosenNum == 3)) {
-                    System.out.println("Again!");
-                } else {
-                    System.out.println("You chose the wrong number! Game Over :(");
-                    System.out.println("You were so close...");
+                if (chosenNum == randomNum || chosenNum < 1 || chosenNum > 3) {
+                    System.out.println("You chose the wrong number! You have 10,000 dollars now!");
+                    System.out.println("Game Over :(");
                     System.exit(0);
                 }
-                loop --;
-            } else if (loop == 1) {
-                int randomNum = (int) (Math.random() * 2) + 1;
-                System.out.println("Choose a number between 1 and 2");
-                int chosenNum = scan.nextInt();
-                if (chosenNum != randomNum && (chosenNum == 1 || chosenNum == 2)) {
-                    System.out.println("Again!");
-                } else {
-                    System.out.println("You chose the wrong number! Game Over :(");
-                    System.out.println("Right on the last one...");
+
+                System.out.println("Again!");
+                loop--;
+                }
+                else if (loop == 1) {
+                    int randomNum = (int) (Math.random() * 2) + 1;
+                     System.out.println("Choose a number between 1 and 2");
+                    int chosenNum = scan.nextInt();
+                if (chosenNum == randomNum || chosenNum < 1 || chosenNum > 2) {
+                    System.out.println("So close, but you still have 100,000 :)");
                     System.exit(0);
                 }
-                loop --;
-            }
+
+                System.out.println("Again!");
+                loop--;
+                }
             end = loop-1;
+            System.out.println("You win the million dollars :)");
         }
+
     }
     public void calculatePercentages () {
-        double result = .2;
+        double result = 1;
         for (int length = 0; length < end - 1; length ++) {
             if (length == 1 || length == 2) {
-                result *= .8;
+                result *= .2;
             } else if (length == 3 || length == 4 || length == 5) {
-                result *= .75;
+                result *= .25;
             } else if (length == 6 || length == 7) {
-                result *= .67;
+                result *= .33;
             } else if (length == 8) {
                 result *= .5;
             }
-            if (length == end) {
+            if (length == end - 1) {
                 result *= .2;
             }
         }
-        System.out.println("It was a " + result + "chance you lost!");
+        System.out.println("It was a " + (result * 100) + "% chance you lost!");
     }
 }
